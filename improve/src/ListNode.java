633cb4a56e9eb43f61e2149bd7036a40c19e0e76
learnm3 <1106015380@qq.com>
2026-04-07 17:27:20 +0800

    first save

---
+++ improve/src/ListNode.java
@@ -0,0 +1,8 @@
+// 在 Solution 类的上方，添加以下代码
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
