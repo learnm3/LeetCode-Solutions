78f30787003b75d0a1178626771e658d0d61f15f
learnm3 <1106015380@qq.com>
2026-04-14 00:01:29 +0800

    4.13

--- improve/src/6.z-字形变换.cpp
+++ improve/src/6.z-字形变换.cpp
@@ -1,4 +1,4 @@
-/*
+5 /*
  * @lc app=leetcode.cn id=6 lang=cpp
  *
  * [6] Z 字形变换
@@ -8,7 +8,19 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        
+        if(numRows == 1) return s;
+        vector<string> rows(numRows);
+        int i= 0,flag = -1;
+        for(char c : s) {
+            rows[i] += c;
+            if(i == 0 || i == numRows - 1) flag = -flag;
+            i += flag;
+        }
+        string result;
+        for(const string& row : rows) {
+            result += row;
+        }
+        return result;
     }
 };
 // @lc code=end
