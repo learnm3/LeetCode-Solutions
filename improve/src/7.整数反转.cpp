9005853b16b567909c4b0f3d165e072b6c1c7e2f
learnm3 <1106015380@qq.com>
2026-04-15 00:10:37 +0800

    4.14

---
+++ improve/src/7.整数反转.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while(x!= 0) {
+            int pop = x % 10;
+            x /= 10;
+            if(res > INT_MAX/10 || (res == INT_MAX / 10 && pop > 7)) return 0;
+            if(res < INT_MIN/10 || (res == INT_MIN / 10 && pop < -8)) return 0;
+            res = res * 10 + pop;
+        }
+        return res;
+    }
+};
+// @lc code=end
