93728a886b81392652ad84d26841b9094eb18558
NoFinalLevel <1106015380@qq.com>
2026-06-10 17:25:27 +0800

    清理：只保留 C++ 力扣题解代码，删除所有非 C++ 文件

--- improve/src/ListNode.java
+++
@@ -1,8 +0,0 @@
-// 在 Solution 类的上方，添加以下代码
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
